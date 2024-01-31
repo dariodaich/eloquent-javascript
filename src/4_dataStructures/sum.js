eb77bd05558900e8606df6bb1fa58df7d29358c2
Dario Daic <dario@daic.me>
2024-02-01 00:16:10 +0100

    Extract additional implementation of sum function

--- src/4_dataStructures/sum.js
+++ src/4_dataStructures/sum.js
@@ -1,11 +1,11 @@
 export function sum(array) {
-  // Implementation#1 - using 'for'
   let total = 0;
   for (let i = 0; i < array.length; i++) {
     total += array[i];
   }
   return total;
+}
 
-  // Implementation#2 - using #reduce
-  // return array.reduce((n, acc) => acc + n, 0);
+export function sumWithReduce(array) {
+  return array.reduce((n, acc) => acc + n, 0);
 }
